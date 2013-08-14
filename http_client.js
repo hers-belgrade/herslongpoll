b4dd6e75acc778844055aa516dd5eb5f2c05974d
veljkopopovic <vuvej@beotel.net>
2013-08-14 12:47:14 +0200

    NOT DONE YET...

--- http_client.js
+++ http_client.js
@@ -1,6 +1,7 @@
 HTTP_LongPollClient = function (url,id_params,cb_map) {
 	var self = this;
 	var url = url || {};
+	self.id_params = id_params;
 
 	var address = url.address || 'localhost';
 	var port = url.port || 80;
@@ -33,10 +34,16 @@ HTTP_LongPollClient = function (url,id_params,cb_map) {
 		var self = this;
 		var data = {};
 
+		// TODO: ovde ti treba sid ako od toga zavisis ....
 		if (!consumer.sid_name) {
+			for (var i in id_params) data[i] = id_params[i];
+			/*
 			data.name = id_params.name;
 			data.roles= id_params.roles;
+			*/
 		}else{
+			data.sid = id_params.sid;
+
 			data[consumer.sid_name] = consumer.sid;
 		}
 
@@ -66,6 +73,7 @@ HTTP_LongPollClient = function (url,id_params,cb_map) {
 				error_reconnect_sec++;
 			}
 
+			delete consumer.sid;
 			console.log('will try again in '+error_reconnect_sec+' seconds');
 			error_to = setTimeout(function () {self.check()}, error_reconnect_sec*1000);
 		});
@@ -77,6 +85,7 @@ HTTP_LongPollClient = function (url,id_params,cb_map) {
 		var command = '/'+request;
 		data = data || {};
 		data[consumer.sid_name] = consumer.sid;
+		data.sid = self.id_params.sid;
 		var request = new Request (schema, address, port, command, method, data, function (resp) {}, function () {
 			console.log(' ERROR CALL BACK .... STA SAD ?', arguments);
 		});
